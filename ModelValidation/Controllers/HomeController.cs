a0e2def77e016833360ac912c24b4338a7cdb523
worknhard99 <fredhausmann@me.com>
2016-06-09 12:32:44 -0400

    First Commit After Creation

---
+++ ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+using ModelValidation.Models;
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            // statements to store new Appointment in a
+            // repository would go here in a real project
+            return View("Completed", appt);
+        }
+    }
+}
