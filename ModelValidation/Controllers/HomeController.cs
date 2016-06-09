5e034294b33eb478375f4a41cb8cd21acc5f9667
worknhard99 <fredhausmann@me.com>
2016-06-09 16:09:38 -0400

    Add JQuery packages and updated two other

--- ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/Controllers/HomeController.cs
@@ -12,24 +12,6 @@ namespace ModelValidation.Controllers
         [HttpPost]
         public ViewResult MakeBooking(Appointment appt)
         {
-            if (string.IsNullOrEmpty(appt.ClientName))
-            {
-                ModelState.AddModelError("ClientName", "Please enter your name");
-            }
-            if (ModelState.IsValidField("Date") && DateTime.Now > appt.Date)
-            {
-                ModelState.AddModelError("Date", "Please enter a date in the future");
-            }
-            if (!appt.TermsAccepted)
-            {
-                ModelState.AddModelError("TermsAccepted", "You must accept the terms");
-            }
-            if (ModelState.IsValidField("ClientName") && ModelState.IsValidField("Date")
-                    && appt.ClientName == "Joe" && appt.Date.DayOfWeek == DayOfWeek.Monday)
-            {
-                ModelState.AddModelError("", "Joe cannot book appointments on Mondays");
-            }
-
             if (ModelState.IsValid)
             {
                 // statements to store new Appointment in a
--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -1,21 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using ModelValidation.Infrastructure;
 
 namespace ModelValidation.Models
 {
-    public class Appointment
+    public class Appointment : IValidatableObject
     {
-        [Required]
         public string ClientName { get; set; }
-
-
         [DataType(DataType.Date)]
-        [Required(ErrorMessage = "Please enter a date")]
         public DateTime Date { get; set; }
-
-        //[Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms")]
-        [MustBeTrue(ErrorMessage = "You must accept the terms")]
         public bool TermsAccepted { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext
+        validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+            if (string.IsNullOrEmpty(ClientName))
+            {
+                errors.Add(new ValidationResult("Please enter your name"));
+            }
+            if (DateTime.Now > Date)
+            {
+                errors.Add(new ValidationResult("Please enter a date in the future"));
+            }
+            if (errors.Count == 0 && ClientName == "Joe"
+            && Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                errors.Add(
+                new ValidationResult("Joe cannot book appointments on Mondays"));
+            }
+            if (!TermsAccepted)
+            {
+                errors.Add(new ValidationResult("You must accept the terms"));
+            }
+            return errors;
+        }
     }
 }
