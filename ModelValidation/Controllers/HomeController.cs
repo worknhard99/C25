867d016f80424989c35c86fbcabd727b433ae703
worknhard99 <fredhausmann@me.com>
2016-06-09 20:38:26 -0400

    Initial Commit

--- ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/Controllers/HomeController.cs
@@ -23,5 +23,23 @@ namespace ModelValidation.Controllers
                 return View();
             }
         }
+        public JsonResult ValidateDate(string Date)
+        {
+            DateTime parsedDate;
+            if (!DateTime.TryParse(Date, out parsedDate))
+            {
+                return Json("Please enter a valid date (mm/dd/yyyy)",
+                JsonRequestBehavior.AllowGet);
+            }
+            else if (DateTime.Now > parsedDate)
+            {
+                return Json("Please enter a date in the future", JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+        }
+
     }
 }
--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -1,39 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using ModelValidation.Infrastructure;
+using System.Web.Mvc;
 
 namespace ModelValidation.Models
 {
-    public class Appointment : IValidatableObject
+    public class Appointment 
     {
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
         public string ClientName { get; set; }
+
+
         [DataType(DataType.Date)]
+        [Remote("ValidateDate", "Home")]
         public DateTime Date { get; set; }
+
         public bool TermsAccepted { get; set; }
-        public IEnumerable<ValidationResult> Validate(ValidationContext
-        validationContext)
-        {
-            List<ValidationResult> errors = new List<ValidationResult>();
-            if (string.IsNullOrEmpty(ClientName))
-            {
-                errors.Add(new ValidationResult("Please enter your name"));
-            }
-            if (DateTime.Now > Date)
-            {
-                errors.Add(new ValidationResult("Please enter a date in the future"));
-            }
-            if (errors.Count == 0 && ClientName == "Joe"
-            && Date.DayOfWeek == DayOfWeek.Monday)
-            {
-                errors.Add(
-                new ValidationResult("Joe cannot book appointments on Mondays"));
-            }
-            if (!TermsAccepted)
-            {
-                errors.Add(new ValidationResult("You must accept the terms"));
-            }
-            return errors;
-        }
     }
 }
